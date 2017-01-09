7de169f68651acc55050ac37f0253e97d6a9d054
Michael Irby <michael.irbstoomuch@gmail.com>
2017-01-09 16:24:29 -0500

    temperature react added

---
+++ react-forms.jsx
@@ -0,0 +1,35 @@
+function Name(props){
+	return(
+		<p>{props.textToDisplay}</p>
+	)
+}
+
+var MyForm = React.createClass({
+
+	getInitialState: function() {
+		return{
+			value: ''
+		}
+	},
+
+	handleChange: function(event){
+		this.setState({
+			value: event.target.value
+		})
+	},
+
+	render: function(){
+		return(
+			<form>
+				<label>Name:</label>
+				<input type="text" placeholder="Enter Your Name" onChange={this.handleChange} />
+				<Name textToDisplay={this.state.value} />
+			</form>
+		)
+	}
+});
+
+ReactDOM.render(
+	<MyForm />,
+	document.getElementById('form-container')
+)
